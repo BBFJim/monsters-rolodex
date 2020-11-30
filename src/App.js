dc9aacd6568cc98a70a9a4519bd6b7aee64ed11b
Billy <bward578@googlemail.com>
2020-11-30 11:20:13 +0000

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/SearchBox";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(response =>
-      response.json())
-      .then(users => this.setState({
-        monsters: users
-      }));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState({
+          monsters: users,
+        })
+      );
   }
 
   render() {
-    const { monsters } = this.state;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(
+      (m) =>
+        m.name.toLowerCase().includes(searchField) ||
+        m.email.toLowerCase().includes(searchField)
+    );
 
     return (
       <div className="App">
-        <CardList monsters={monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "./card-styles.css";
 
-export const Card = props => {
-    console.log(props);
-
-    return (
-        <div className="card-container">
-            <h1>{props.monster.name}</h1>
-        </div>
-    )
-}
+export const Card = props =>
+    <div className="card-container">
+        <h1>{props.monster.name}</h1>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1`} style={{ height: "150px", width: "150px", }} />
+        <p>{props.monster.email}</p>
+    </div>
